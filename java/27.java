70321186ee5ff4504a6289057156308c9ed34aa7
Qinghao Dai <rogerdai16@gmail.com>
2015-05-23 20:09:26 -0700

    easy one, remove elements.

---
+++ java/27.java
@@ -0,0 +1,19 @@
+public class Solution {
+    private void swap(int[] nums, int i, int j){
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+    
+    public int removeElement(int[] nums, int val) {
+        if(nums == null) return 0;
+        int j = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != val){
+                swap(nums, i, j);
+                j++;
+            }
+        }
+        return j;
+    }
+}

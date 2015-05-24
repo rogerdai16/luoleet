18b6cd4a62800db62f248cd71b738b5262d568c5
Qinghao Dai <rogerdai16@gmail.com>
2015-05-23 21:42:36 -0700

    strstr, details needs attention.

---
+++ java/28.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int strStr(String haystack, String needle) {
+        if(haystack == null || needle == null || needle.length() > haystack.length()) return -1;
+        if(needle.length() == 0) return 0;
+        if(haystack.length() == 0 && needle.length() == 0) return 0;
+        for(int i = 0; i < haystack.length() - needle.length() + 1; i++){
+            if(haystack.charAt(i) == needle.charAt(0)){
+                int len = 0;
+                while(len < needle.length() && haystack.charAt(i + len) == needle.charAt(len))len++;
+                if(len == needle.length()) return i;
+            }
+        }
+        return -1;
+    }
+}

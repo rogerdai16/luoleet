54e0f56c56adf096ad2a71eb719d14745ecfb8de
Qinghao Dai <rogerdai16@gmail.com>
2015-05-25 12:46:14 -0700

    count and say, prove that there is no 4in the result.

---
+++ java/38.java
@@ -0,0 +1,27 @@
+public class Solution {
+    private String next(String in){
+        StringBuilder sb = new StringBuilder();
+        int sum = 1;
+        for(int i = 1; i < in.length(); i++){
+            if(in.charAt(i) == in.charAt(i - 1)){
+                sum++;
+            }else{
+                sb.append(sum);
+                sb.append(in.charAt(i - 1));
+                sum = 1;
+            }
+        }
+        sb.append(sum);
+        sb.append(in.charAt(in.length() - 1));
+        return sb.toString();
+    }
+    
+    
+    public String countAndSay(int n) {
+        String rst = "1";
+        while(--n != 0){
+            rst = next(rst);
+        }
+        return rst;
+    }
+}

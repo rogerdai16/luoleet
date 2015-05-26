de8cee5a6a147b6fcea01f7197da169d4d0b070d
Qinghao Dai <rogerdai16@gmail.com>
2015-05-26 08:56:06 -0700

    good solution to permutation.

---
+++ java/46.java
@@ -0,0 +1,32 @@
+public class Solution {
+    
+    private ArrayList<ArrayList<Integer>> permuteHelper(ArrayList<Integer> all){
+        ArrayList<ArrayList<Integer>> localRst = new ArrayList<ArrayList<Integer>>();
+        if(all.size() == 0) return localRst;
+        if(all.size() == 1){
+            localRst.add(all);
+            return localRst;
+        }
+        for(int i = 0; i < all.size(); i++){
+            Integer curr = all.remove(i);
+            ArrayList<ArrayList<Integer>> subRst = permuteHelper(all);
+            for(int j = 0; j < subRst.size(); j++){
+                ArrayList tmp = new ArrayList<Integer>(subRst.get(j));
+                tmp.add(0, curr);
+                localRst.add(tmp);
+            }
+            all.add(i, curr);
+        }
+        return localRst;
+        
+    }
+    
+    public ArrayList<ArrayList<Integer>> permute(int[] nums) {
+        ArrayList<ArrayList<Integer>> rst = new ArrayList<ArrayList<Integer>>();
+        if(nums == null || nums.length == 0) return rst;
+        ArrayList<Integer> all = new ArrayList<Integer>();
+        for(int i : nums) all.add(i);
+        rst = permuteHelper(all);
+        return rst;
+    }
+}

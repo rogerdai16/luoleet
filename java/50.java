5e8563f2bfe7c8f2b603e185fa9c807c8be6a436
Qinghao Dai <rogerdai16@gmail.com>
2015-05-26 10:06:22 -0700

    pow, use long to avoid int overflow.

---
+++ java/50.java
@@ -0,0 +1,17 @@
+public class Solution {
+    
+    private double myPowHelper(double x, long n){
+        if(n == 0) return 1d;
+        if(n == 1) return x;
+        if(n < 0) return 1 / myPowHelper(x, -n);
+        long half = n / 2;
+        double value = myPowHelper(x, half);
+        if(n % 2 == 0) return value * value;
+        else return value * value * x;
+    }
+    
+    
+    public double myPow(double x, int n) {
+        return myPowHelper(x, (long)n);
+    }
+}

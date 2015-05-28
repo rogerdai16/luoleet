a21733229fc9ca71aabc07f4d9ff677aa5945f26
Qinghao Dai <rogerdai16@gmail.com>
2015-05-27 23:36:09 -0700

    minimum depth of binary tree, a little different from max depth of binary tree.

---
+++ java/111.java
@@ -0,0 +1,8 @@
+public class Solution {
+    public int minDepth(TreeNode root) {
+        if(root == null) return 0;
+        if(root.left == null && root.right != null) return minDepth(root.right) + 1;
+        if(root.left != null && root.right == null) return minDepth(root.left) + 1;
+        return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+    }
+}

991364f84b8d63f8f6ec177689fbe13444272a75
Qinghao Dai <rogerdai16@gmail.com>
2015-05-31 12:16:29 -0700

    from 0 to length - 1.

--- java/91.java
+++ java/91.java
@@ -3,9 +3,9 @@ class Solution{
         if(s == null || s.length() == 0 || s.charAt(0) == '0') return 0;
         int prev = 0;
         int curr = 1;
-        for(int i = 1; i <= s.length(); i++){ // same as fibonacci
+        for(int i = 0; i < s.length(); i++){ // same as fibonacci
             if(s.charAt(i) == '0') curr = 0;
-            if(i < 2 || !(s.charAt(i - 2) == '1' || (s.charAt(i - 2) == '2' && s.charAt(i - 1) <= '6'))) prev = 0;
+            if(i < 1 || !(s.charAt(i - 1) == '1' || (s.charAt(i - 1) == '2' && s.charAt(i) <= '6'))) prev = 0;
             if(curr == 0 && prev == 0) reutrn 0;
             int tmp = curr;
             curr = curr + prev;

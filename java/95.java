30af68a341bb0d2ac17127b865ee89dc33ca1174
Qinghao Dai <rogerdai16@gmail.com>
2015-06-01 00:33:42 -0700

    Update 95.java
    beautify

--- java/95.java
+++ java/95.java
@@ -1,16 +1,16 @@
 public class Solution {
 
-	private LinkedList<TreeNode> generate(int start, int end){
-		LinkedList<TreeNode> rst = new LinkedList<TreeNode>();
-		if(start > end){
+	private LinkedList < TreeNode > generate(int start, int end) {
+		LinkedList < TreeNode > rst = new LinkedList < TreeNode > ();
+		if (start > end) {
 			rst.addLast(null);
 			return rst;
 		}
-		for(int k = start; k <= end; k++){
-			LinkedList<TreeNode> leftSubTrees = generate(start, k - 1);
-			LinkedList<TreeNode> rightSubTrees = generate(k + 1, end);
-			for(TreeNode left : leftSubTrees){
-				for(TreeNode right : rightSubTrees){
+		for (int k = start; k <= end; k++) {
+			LinkedList < TreeNode > leftSubTrees = generate(start, k - 1);
+			LinkedList < TreeNode > rightSubTrees = generate(k + 1, end);
+			for (TreeNode left: leftSubTrees) {
+				for (TreeNode right: rightSubTrees) {
 					TreeNode curr = new TreeNode(k);
 					curr.left = left;
 					curr.right = right;
@@ -22,8 +22,8 @@ public class Solution {
 	}
 
 
-    public LinkedList<TreeNode> generateTrees(int n) {
-        if(n <= 0) return generate(1, n);
+	public LinkedList < TreeNode > generateTrees(int n) {
+		if (n <= 0) return generate(1, n);
 		return generate(1, n);
-    }
+	}
 }

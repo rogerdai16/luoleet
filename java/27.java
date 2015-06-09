a3eb5079bfdeb3fb5e57713c38d025c67be40a63
Qinghao Dai <rogerdai16@gmail.com>
2015-06-09 00:07:14 -0700

    remove element, from end to start

--- java/27.java
+++ java/27.java
@@ -17,3 +17,23 @@ public class Solution {
         return j;
     }
 }
+// from end to start, a little difference
+public class Solution {
+    private void swap(int[] nums, int i, int j){
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+    
+    public int removeElement(int[] nums, int val) {
+        if(nums == null) return 0;
+        int j = 0;
+        for(int i = nums.length - 1; i >= j; i--){
+            if(nums[i] != val){
+                swap(nums, i++, j);
+                j++;
+            }
+        }
+        return j;
+    }
+}

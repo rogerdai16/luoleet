f4d7aca6506b1381da96cd955285c73a949c40a2
Qinghao Dai <rogerdai16@gmail.com>
2015-06-13 20:51:13 -0700

    new permutations.

--- java/46.java
+++ java/46.java
@@ -1,3 +1,20 @@
+/*
+void permute(char *a, int l, int r)
+{
+   int i;
+   if (l == r)
+     printf("%s\n", a);
+   else
+   {
+       for (i = l; i <= r; i++)
+       {
+          swap((a+l), (a+i));
+          permute(a, l+1, r);
+          swap((a+l), (a+i)); //backtrack
+       }
+   }
+}
+*/
 public class Solution {
     
     private ArrayList<ArrayList<Integer>> permuteHelper(ArrayList<Integer> all){

55e21c7e8c83f280c10c337263102f620a854e08
Qinghao Dai <rogerdai16@gmail.com>
2015-06-14 17:55:54 -0700

    Update 97.java
    tab

--- java/97.java
+++ java/97.java
@@ -6,8 +6,8 @@ public class Solution {
 		for (int i = 1; i <= s1.length(); i++) dp[i][0] = dp[i - 1][0] && s1.charAt(i - 1) == s3.charAt(i - 1);
 		for (int j = 1; j <= s2.length(); j++) dp[0][j] = dp[0][j - 1] && s2.charAt(j - 1) == s3.charAt(j - 1);
 		for (int i = 1; i <= s1.length(); i++)
-		for (int j = 1; j <= s2.length(); j++)
-		dp[i][j] = (s1.charAt(i - 1) == s3.charAt(i + j - 1) && dp[i - 1][j]) || (s2.charAt(j - 1) == s3.charAt(i + j - 1) && dp[i][j - 1]);
+			for (int j = 1; j <= s2.length(); j++)
+				dp[i][j] = (s1.charAt(i - 1) == s3.charAt(i + j - 1) && dp[i - 1][j]) || (s2.charAt(j - 1) == s3.charAt(i + j - 1) && dp[i][j - 1]);
 		return dp[s1.length()][s2.length()];
 	}
 }

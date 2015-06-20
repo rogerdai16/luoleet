df037779dcb4ef437176079888d6b53defbe9922
Qinghao Dai <rogerdai16@gmail.com>
2015-06-20 13:03:45 -0700

    startbucks

--- java/66.java
+++ java/66.java
@@ -1,16 +1,16 @@
-	public class Solution {
-		public int[] plusOne(int[] digits) {
-			LinkedList < Integer > rst = new LinkedList < Integer > ();
-			if (digits == null || digits.length == 0) return digits;
-			int carry = 1;
-			for (int idx = digits.length - 1; idx >= 0; idx--) {
-				rst.addFirst((digits[idx] + carry) % 10);
-				carry = (digits[idx] + carry) / 10;
-			}
-			if (carry != 0) rst.addFirst(carry);
-			int[] arr = new int[rst.size()];
-			int i = 0;
-			for (Integer e: rst) arr[i++] = e;
-			return arr;
-		}
-	}
+public class Solution {
+    public int[] plusOne(int[] digits) {
+        LinkedList < Integer > rst = new LinkedList < Integer > ();
+        if (digits == null || digits.length == 0) return digits;
+        int carry = 1;
+        for (int idx = digits.length - 1; idx >= 0; idx--) {
+            rst.addFirst((digits[idx] + carry) % 10);
+            carry = (digits[idx] + carry) / 10;
+        }
+        if (carry != 0) rst.addFirst(carry);
+        int[] arr = new int[rst.size()];
+        int i = 0;
+        for (Integer e: rst) arr[i++] = e;
+        return arr;
+    }
+}

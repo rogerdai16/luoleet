11a7341ef5a38e539f73c4404d9f0755a3465581
Qinghao Dai <rogerdai16@gmail.com>
2015-06-23 23:15:43 -0700

    a better count prime solution.

--- java/204.java
+++ java/204.java
@@ -1,21 +1,21 @@
 public class Solution {
     public int countPrimes(int n) {
-        if(n <= 0)return 0;
+        if (n <= 2) return 0;
         boolean[] nonPrime = new boolean[n];
+        int rst = n - 2;
         int i = 2;
-        while(i < Math.sqrt(n)){
-            if(nonPrime[i - 1] == false){
+        while (i < Math.sqrt(n)) {
+            if (nonPrime[i - 1] == false) {
                 int j = i * 2;
-                while(j <= n){
+                while (j <= n) {
+                    if (j != n && nonPrime[j - 1] == false) rst--;
                     nonPrime[j - 1] = true;
                     j += i;
                 }
             }
-            if(i != 2) i += 2;
+            if (i != 2) i += 2;
             else i++;
         }
-        int count = 0;
-        for(i = 2; i < n; i++) if(nonPrime[i - 1] == false) count++;
-        return count;
+        return rst;
     }
 }

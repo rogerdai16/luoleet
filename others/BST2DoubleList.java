f2e60c748cbb7644583843b247d1c2802cb7828f
Qinghao Dai <rogerdai16@gmail.com>
2015-06-25 00:37:48 -0700

    BST to doublely linkedlist

---
+++ others/BST2DoubleList.java
@@ -0,0 +1,48 @@
+/**
+ * Created by Qinghao on 6/24/2015.
+ */
+public class BST2DoubleList {
+    class TreeNode{
+        TreeNode left;
+        TreeNode right;
+        TreeNode prev;
+        TreeNode next;
+        int val;
+        public TreeNode(int v){
+            val = v;
+        }
+    }
+
+    // do the inorder traverse and link them
+    private TreeNode prev = null;
+    private TreeNode head = null;
+
+    private void inorder(TreeNode root){
+        if(root == null) return;
+        inorder(root.left);
+        if(prev == null) {
+            head = root;
+        }else{
+            prev.next = root;
+            root.prev = prev;
+        }
+        prev = root;
+        inorder(root.right);
+    }
+
+    public TreeNode convert(TreeNode root){
+        inorder(root);
+        if(head == null) return null;
+        head.prev = prev;
+        prev.next = head;
+        return head;
+    }
+
+    public static void main(String[] args){
+        BST2DoubleList me = new BST2DoubleList();
+        TreeNode root = me.new TreeNode(2);
+        root.left = me.new TreeNode(1);
+        root.right = me.new TreeNode(3);
+        TreeNode head = me.convert(root);
+    }
+}

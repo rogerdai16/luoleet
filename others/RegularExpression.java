662deb7e339b8a26bae204215459e6a1e4ae13b6
Qinghao Dai <rogerdai16@gmail.com>
2015-06-25 15:38:53 -0700

    revise regular expression matching

--- others/RegularExpression.java
+++ others/RegularExpression.java
@@ -10,24 +10,19 @@ public class RegularExpression {
         }
 
         private boolean _isMatch(String s, int sPos, String p, int pPos) {
-            if (sPos == s.length() || pPos == p.length()) {
-                if (sPos == s.length() && pPos == p.length()) return true; // to the end
-                if (sPos == s.length() && pPos < p.length() - 1 && p.charAt(pPos + 1) == '*') return _isMatch(s, sPos, p, pPos + 2);
+            if (sPos >= s.length() || pPos >= p.length()) {
+                if (sPos >= s.length() && pPos >= p.length()) return true;
+                if (sPos >= s.length() && pPos < p.length() - 1 && p.charAt(pPos + 1) == '*')
+                    return _isMatch(s, sPos, p, pPos + 2);
                 return false;
             }
-            //not match at current position, if no *, then match failed
-            //Otherwise we can skip the current * in the regex and continue
-            //to next match.
             if (s.charAt(sPos) != p.charAt(pPos) && p.charAt(pPos) != '.') {
                 //no *, match failed
-                if (pPos == p.length() - 1 || p.charAt(pPos + 1) != '*')
+                if (pPos < p.length() - 1 && p.charAt(pPos + 1) != '*')
                     return false;
                 //skip the *
                 return _isMatch(s, sPos, p, pPos + 2);
             }
-            //match at current position, if no *, then continue to match the next
-            //position both in text and regex.
-            //Otherwise try to match one more chars and provide states for backtrack
             if (pPos == p.length() - 1 || p.charAt(pPos + 1) != '*') return _isMatch(s, sPos + 1, p, pPos + 1);
             return _isMatch(s, sPos + 1, p, pPos) || _isMatch(s, sPos, p, pPos + 2);
         }

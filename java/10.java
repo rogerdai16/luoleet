041dbffc1376fe6f41c895de86ff268154adcb94
Qinghao Dai <rogerdai16@gmail.com>
2015-06-25 16:23:10 -0700

    update regular expression matching, NO.10 problem.

--- java/10.java
+++ java/10.java
@@ -1,16 +1,22 @@
 public class Solution {
-	public boolean isMatch(String s, String p) {
-		if (p.contains(".") || p.contains("*")) {
-			if (p.length() == 1 || p.charAt(1) != '*') return comp(s, p, 0) && isMatch(s.substring(1), p.substring(1));
-			for (int i = 0; i == 0 || comp(s, p, i - 1); i++) {
-				if (isMatch(s.substring(i), p.substring(2))) return true;
-			}
-			return false;
-		}
-		return s.equals(p);
-	}
+    public boolean isMatch(String s, String p) {
+        return _isMatch(s, 0, p, 0);
+    }
 
-	private boolean comp(String s, String p, int i) {
-		return i < s.length() && (p.charAt(0) == s.charAt(i) || p.charAt(0) == '.');
-	}
+    private boolean _isMatch(String s, int sPos, String p, int pPos) {
+        if (sPos >= s.length() || pPos >= p.length()) {
+            if (sPos >= s.length() && pPos >= p.length()) return true;
+            if (sPos >= s.length() && pPos < p.length() - 1 && p.charAt(pPos + 1) == '*')
+                return _isMatch(s, sPos, p, pPos + 2);
+            return false;
+        }
+        if (s.charAt(sPos) != p.charAt(pPos) && p.charAt(pPos) != '.') {
+            //no *, match failed
+            if (pPos < p.length() - 1 && p.charAt(pPos + 1) == '*')
+                return _isMatch(s, sPos, p, pPos + 2);
+            return false;
+        }
+        if (pPos == p.length() - 1 || p.charAt(pPos + 1) != '*') return _isMatch(s, sPos + 1, p, pPos + 1);
+        return _isMatch(s, sPos + 1, p, pPos) || _isMatch(s, sPos, p, pPos + 2);
+    }
 }

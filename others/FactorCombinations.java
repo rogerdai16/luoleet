8bbd00f02aeab82a9686b86d233d3c136b5b94a2
Qinghao Dai <rogerdai16@gmail.com>
2015-06-25 17:33:14 -0700

    factor combination.

---
+++ others/FactorCombinations.java
@@ -0,0 +1,36 @@
+import java.util.LinkedList;
+
+/**
+ * Created by Qinghao on 6/25/2015.
+ */
+public class FactorCombinations {
+
+    private static void dfs(LinkedList<Integer> path, int n) {
+        if (n == 1) {
+            for (Integer i : path) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+            return;
+        }
+        for (int i = path.isEmpty() ? 2 : path.peekLast(); i <= n; i++) {
+            if (n % i == 0) {
+                path.addLast(i);
+                dfs(path, n / i);
+                path.removeLast();
+            }
+        }
+    }
+
+    public static void factorCombinations(int n) {
+        if (n <= 1) return;
+        LinkedList<Integer> path = new LinkedList<Integer>();
+        dfs(path, n);
+    }
+
+
+    public static void main(String[] args) {
+//        System.out.println(~(-0) + 1); // take zero, flip all the bits and plus one, get zero
+        factorCombinations(28);
+    }
+}

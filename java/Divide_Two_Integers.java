176468d5540e1d6c606684ef5e3ade615f0ff9d0
Qinghao Dai <rogerdai16@gmail.com>
2015-06-27 22:02:45 -0700

    Update Divide_Two_Integers.java
    shorten divide

--- java/Divide_Two_Integers.java
+++ java/Divide_Two_Integers.java
@@ -2,9 +2,7 @@ public class Solution {
     public int divide(int dividend, int divisor) {
         long rst = 0;
         boolean negative = false;
-        if(Math.abs(Math.signum(dividend) - Math.signum(divisor)) >= 2){
-            negative = true;
-        }
+        if(Math.abs(Math.signum(dividend) - Math.signum(divisor)) >= 2)negative = true;
         long a = Math.abs((long)dividend);
         long b = Math.abs((long)divisor);
         while(a >= b){
@@ -17,12 +15,8 @@ public class Solution {
                 loop++;
             }
         }
-        if(negative == false && rst >= Integer.MAX_VALUE){
-            return Integer.MAX_VALUE;
-        }
-        if(negative && -rst <= Integer.MIN_VALUE){
-            return Integer.MIN_VALUE;
-        }
+        if(negative == false && rst >= Integer.MAX_VALUE) return Integer.MAX_VALUE;
+        if(negative && -rst <= Integer.MIN_VALUE) return Integer.MIN_VALUE;
         return negative? (int)-rst : (int)rst;
     }
 }

230c0506bd8d5c82214214df5bfc11f2ebc64701
Qinghao Dai <rogerdai16@gmail.com>
2015-07-01 00:05:35 -0700

    regular expression dp

--- others/RegularExpression.java
+++ others/RegularExpression.java
@@ -25,12 +25,37 @@ public class RegularExpression {
             if (pPos == p.length() - 1 || p.charAt(pPos + 1) != '*') return _isMatch(s, sPos + 1, p, pPos + 1);
             return _isMatch(s, sPos + 1, p, pPos) || _isMatch(s, sPos, p, pPos + 2);
         }
+
+
+        // dp solution
+        private boolean isMatchDP(String s, String p){
+            int m = s.length();
+            int n = p.length();
+            boolean[][] dp = new boolean[m + 1][n + 1];
+            for(int i = 0; i <= m; i++)
+                for(int j = 0; j <= n; j++){
+                    if(j == 0){
+                        dp[i][j] = i == 0;
+                    }else if(i == 0){
+                        if(j > 1 && p.charAt(j - 1) == '*') dp[i][j] = dp[i][j - 1] || dp[i][j - 2];
+                    }else{
+                        char sc = s.charAt(i - 1);
+                        char pc = s.charAt(j - 1);
+                        if(sc == pc || pc == '.') dp[i][j] = dp[i - 1][j - 1];
+                        else if(j > 1 && pc == '*'){
+                            if(((sc == p.charAt(j - 2) || p.charAt(j - 2) == '.')) && dp[i - 1][j]) dp[i][j] = true;// match previous one
+                            else dp[i][j] = dp[i][j - 2];
+                        }
+
+                    }
+                }
+            return dp[m][n];
+        }
     }
 
     public static void main(String[] args){
         RegularExpression me = new RegularExpression();
         Solution s = me.new Solution();
-//        System.out.println(s.isMatch("abcd", "abcd"));
         System.out.println(s.isMatch("abcd", "a.*A*"));
     }
 }

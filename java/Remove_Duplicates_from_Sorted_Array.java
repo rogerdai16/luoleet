ddda5c652b0812d5d15f8d4c551591f7920b23da
Qinghao Dai <rogerdai16@gmail.com>
2015-07-10 10:46:28 -0700

    remove duplicate from sorted array

--- java/Remove_Duplicates_from_Sorted_Array.java
+++ java/Remove_Duplicates_from_Sorted_Array.java
@@ -1,20 +1,10 @@
 public class Solution {
-    private void swap(int[] nums, int i, int j){
-        int tmp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = tmp;
-    }
-    
     public int removeDuplicates(int[] nums) {
-        if(nums == null || nums.length < 1) return 0;
-        if(nums.length == 1) return 1;
-        int j = 1;
-        for(int i = 1; i < nums.length; i++){
-            if(nums[i] != nums[j - 1]){
-                swap(nums, i, j);
-                j++;
-            }
+        if(nums == null || nums.length == 0) return 0;
+        int idx = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(idx == 0 || nums[i] != nums[idx - 1]) nums[idx++] = nums[i];
         }
-        return j;
+        return idx;
     }
 }

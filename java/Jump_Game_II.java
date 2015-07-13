80d8b53731cf89128c30284b1d4209d84420487b
Qinghao Dai <rogerdai16@gmail.com>
2015-07-12 18:02:49 -0700

    read4k II

--- java/Jump_Game_II.java
+++ java/Jump_Game_II.java
@@ -11,6 +11,5 @@ public class Solution {
     		reach = Math.max(reach, i + A[i]);
     	}
     	return count;
-        
     }
 }
--- java/Read_N_Characters_Given_Read4.java
+++ java/Read_N_Characters_Given_Read4.java
@@ -1,8 +1,7 @@
+/* The read4 API is defined in the parent class Reader4.
+      int read4(char[] buf); */
+
 public class Solution extends Reader4 {
-    private char[] buffer = new char[4];
-    private int size = 0;
-    private int offset = 0;
-    
     /**
      * @param buf Destination buffer
      * @param n   Maximum number of characters to read
@@ -11,13 +10,12 @@ public class Solution extends Reader4 {
     public int read(char[] buf, int n) {
         int read = 0;
         boolean eof = false;
+        char[] buffer = new char[4];
         while(eof == false && read < n){
-            int tmp = size > 0? size : read4(buffer);
-            if(size == 0 && tmp == 0) eof = true;
-            int bytes = Math.min(n - read, tmp);
-            System.arraycopy(buffer, offset, buf, read, bytes);
-            offset = (offset + bytes) % 4;
-            size = tmp - bytes;
+            int curr = read4(buffer);
+            if(curr < 4) eof = true;
+            int bytes = Math.min(curr, n - read);
+            System.arraycopy(buffer, 0, buf, read, bytes);
             read += bytes;
         }
         return read;

7de645d33992ecbae3c7f52ffd2de4c5a982367a
czonzhu <czonzhu@gmail.com>
2015-08-03 17:24:17 -0500

    Update 3Sum_Closest.java
    We dont need to consider duplicate in closest problem. This would make the code clean though a bit slower. What do you think about it?

--- java/3Sum_Closest.java
+++ java/3Sum_Closest.java
@@ -1,32 +1,16 @@
 public class Solution {
     public int threeSumClosest(int[] nums, int target) {
-        int diff = Integer.MAX_VALUE;
-        int rst = 0;
-        if(nums == null || nums.length < 3) return diff;
-        Arrays.sort(nums);
-        for(int i = 0; i < nums.length - 2; i++){
-            if( i > 0 && nums[i] == nums[i - 1]) continue;
-            int left = i + 1;
-            int right = nums.length - 1;
-            int curr = nums[i];
-            while(left < right){
-                int gap = Math.abs(curr + nums[left] + nums[right] - target);
-                if(gap < diff){
-                    rst = curr + nums[left] + nums[right];
-                    diff = gap;
-                }
-                if(curr + nums[left] + nums[right] < target){
-                    left++;
-                    while(nums[left] == nums[left - 1] && left < right)left++;
-                }else if(curr + nums[left] + nums[right] > target){
-                    right--;
-                    while(nums[right] == nums[right + 1] && left < right) right--;
-                }else if(curr + nums[left] + nums[right] == target){
-                    left++;
-                    right--;
-                    while(nums[left] == nums[left - 1] && left < right)left++;
-                    while(nums[right] == nums[right + 1] && left < right) right--;
-                }
+        if(nums == null || nums.length < 3) return 0;// Pay attention to the edge case nums.length < 3
+        int rst = nums[0] + nums[1] + nums[nums.length - 1];
+        Arrays.sort(nums);// Never, ever, forget to sort when using two pointers.
+        for(int i = 0; i < nums.length; i++){
+            int l = i + 1, r = nums.length - 1;
+            while(l < r){
+                int sum = nums[l] + nums[r] + nums[i];
+                if(Math.abs(sum - target) < Math.abs(rst - target))
+                 rst = sum;
+                if(sum < target) l++;
+                else r--;
             }
         }
         return rst;

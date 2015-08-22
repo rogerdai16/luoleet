e52be42753494719e7063b095b715d9c602e77ea
Qinghao Dai <rogerdai16@gmail.com>
2015-08-22 07:43:25 -0700

    Update twoSum.scala
    shorter version of two sum in scala

--- scala/twoSum.scala
+++ scala/twoSum.scala
@@ -1,11 +1,8 @@
 def twoSum(arr: Array[Int], target: Int): Boolean = {
     if(arr == null || arr.length == 0) false
 	val hs = new java.util.HashSet[Int]
-	for(ele <- arr) {
-	    if(hs.contains(target - ele)) return true
-		hs.add(ele)
-    }
+	for(ele <- arr) if(hs.contains(target - ele)) return true else hs.add(ele)
 	false
 }
 val test1 = Array(1,2,3,4,55,22,43,2,5214,235,3,42,3423,1423,523)
-println("test 1 is " + twoSum(test1, -1))
+println("test 1 is " + twoSum(test1, 4))
